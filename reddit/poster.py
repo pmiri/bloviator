12b1eeaa4670080a93e7319f2a358722ad981838
pmiri <pmiriello@gmail.com>
2017-03-15 16:46:10 -0400

    #9 a bunch more stuff
    representative of the loop.

--- reddit/poster.py
+++ reddit/poster.py
@@ -1,5 +1,7 @@
 import sys, getopt
 
+import time
+
 import praw
 from praw_object_data import retry_if_broken_connection
 import json
@@ -8,6 +10,7 @@ client_id = "G_rle_KDBCc7kw"
 secret="QjZR808fes7P8AFzUn8mb7WYC0w"
 user_agent = 'reddit posting for bloviator, using praw tutorial'
 
+bot_list = ["left", "right", "apol"]
 #our json file
 json_bot_file = "bots.json"
 
@@ -33,28 +36,36 @@ def get_threads_replied_to(bot_type):
     threadfile = open(bot_type + "_" + post_list_file, 'r')
     return threadfile.readlines()
 
-def main(argv):
+def main():
+    #loop
+    while(True)
+        #TODO: this is a temp replacement for testing
+        bot_list = ["test"]
+        for(bot_type in bot_list)
+            bot = loadbot(bot_type)
+            reddit = poster(bot)
 
-    bot_type = argv[1]
+            #this may be irrelevant givent the nature of the submission stream
+            #posts = get_threads_replied_to(bot_type)
+            for(sub in bot["subreddits"])
+                #NOTE: you can append subreddits by +'ing them together. This will be useful when interacting with many political subs
+                subreddit = reddit.subreddit(sub)
+                t = time.time()
+                for submission in subreddit.stream.submissions():
+                    print('post(s) found')
+                    #check to see if the bot hasn't already replied
+                    if time.time() - t > 60*10 #TODO: figure out appropriate conditions to NOT post submission.id not in posts:
+                        t = time.time()
+                        print('replying in thread' + submission)
+                        #if submission contains what we want, reply.
+                        #TODO: PLUG ML TRAINED ANSWER SCRIPT HERE
+                        submission.reply("REPLY")
 
-    bot = loadbot(bot_type)
-    reddit = poster(bot)
-
-    posts = get_threads_replied_to(bot_type)
-
-    #NOTE: you can append subreddits by +'ing them together. This will be useful when interacting with many political subs
-    subreddit = reddit.subreddit('+'.join(bot["subreddits"]))
-    for submission in subreddit.stream.submissions():
-        print('post(s) found')
-        #check to see if the bot hasn't already replied
-        if submission.id not in posts:
-            print('replying in thread')
-            #if submission contains what we want, reply.
-            submission.reply('YO THIS WAS AUTOMATED. CAN U BEELEEVE THAT?')
-
-            threads = open(bot_type + '_' + post_list_file, 'a')
-            threads.write(submission.id)
+                        threads = open(bot_type + '_' + post_list_file, 'a+')
+                        botlog = open(bot_type + '_comments', 'a+')
+                        threads.write(submission.id)
+                        botlog.write(submission.id + ': ' + 'TODO: comment go here')
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
