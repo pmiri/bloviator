297d0dd097778804fb316c71f94f851711d5b2fc
Matthew Mongrain <matthew.mongrain@gmail.com>
2017-03-15 01:31:17 +0000

    database password changed

--- scraper/dbinfo.py
+++ scraper/dbinfo.py
@@ -1,7 +1,7 @@
-database = 'bloviator'
+database='bloviator'
 tablespace='pg_default'#try pg_default or pg_global if you haven't configured a different one
 username='bloviator'
-password='bloviator_498N'
+password='ohn9baeseeJ6'
 host='localhost'
 port=5432
 dbtype='postgres'
